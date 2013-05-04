7425d20948406acce1d8f3bbdb9582dbb86b7342
Rick Austin <rick@rickaustin.net>
2013-05-04 17:14:03 -0400

    Make a basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121203042353) do
+ActiveRecord::Schema.define(:version => 20130504203113) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
