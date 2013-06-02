3477d0d325bbf1cddd7d31f3d40392b09822e7d3
Rick Austin <rick@rickaustin.net>
2013-06-02 13:04:57 -0400

    Finished chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,15 @@ describe "signup" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content('Password digest') }
+      end
+
     end
 
     describe "with valid information" do
@@ -50,6 +59,7 @@ describe "signup" do
         let(:user) { User.find_by_email("user@example.com") }
 
         it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
 
     end
